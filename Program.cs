23dffd6817f1f10734585492c64bf344b5122126
Tomas Hubelbauer <tom.hubelbauer@microsoft.com>
2019-05-03 13:49:01 +0200

    Demonstrate obsolete way of logging

--- Program.cs
+++ Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Console;
 
 namespace ef_core_logging
 {
@@ -20,12 +22,14 @@ namespace ef_core_logging
 
   public class AppDbContext : DbContext
   {
+    private static readonly LoggerFactory LoggerFactory = new LoggerFactory(new[] { new ConsoleLoggerProvider((_, __) => true, true) });
     public DbSet<Item> Items { get; set; }
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
       optionsBuilder.UseSqlServer($@"Server=(localdb)\{nameof(ef_core_logging)};Database={nameof(ef_core_logging)};");
       optionsBuilder.EnableDetailedErrors();
       optionsBuilder.EnableSensitiveDataLogging();
+      optionsBuilder.UseLoggerFactory(LoggerFactory);
     }
   }
 
--- README.md
+++ README.md
@@ -2,6 +2,6 @@
 
 1. `dotnet new console`
 2. `dotnet add package Microsoft.EntityFrameworkCore.SqlServer`
-3. `<LangVersion>latest</LangVersion>` for `async Task Main`
-4. `sqllocaldb create ef_core_logging`
-5. Set up entities and DB context
+3. `sqllocaldb create ef_core_logging`
+4. Set up entities and DB context
+5. `dotnet add package Microsoft.Extensions.Logging.Console`
