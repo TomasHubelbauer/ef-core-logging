60b98b1b714402cae43eeb01f54a9fd5d37f3fea
Tomas Hubelbauer <tom.hubelbauer@microsoft.com>
2019-05-03 13:45:41 +0200

    Make a bare demo without any logging for now

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace ef_core_logging
+{
+  class Program
+  {
+    static async Task Main(string[] args)
+    {
+      using (var appDbContext = new AppDbContext())
+      {
+        await appDbContext.Database.EnsureDeletedAsync();
+        await appDbContext.Database.EnsureCreatedAsync();
+        await appDbContext.Items.AddAsync(new Item { Name = "TEST" });
+        await appDbContext.SaveChangesAsync();
+      }
+    }
+  }
+
+  public class AppDbContext : DbContext
+  {
+    public DbSet<Item> Items { get; set; }
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+      optionsBuilder.UseSqlServer($@"Server=(localdb)\{nameof(ef_core_logging)};Database={nameof(ef_core_logging)};");
+      optionsBuilder.EnableDetailedErrors();
+      optionsBuilder.EnableSensitiveDataLogging();
+    }
+  }
+
+  public class Item
+  {
+    public Guid Id { get; set; }
+    public string Name { get; set; }
+  }
+}
---
+++ README.md
@@ -0,0 +1,7 @@
+# EF Core Logging
+
+1. `dotnet new console`
+2. `dotnet add package Microsoft.EntityFrameworkCore.SqlServer`
+3. `<LangVersion>latest</LangVersion>` for `async Task Main`
+4. `sqllocaldb create ef_core_logging`
+5. Set up entities and DB context
